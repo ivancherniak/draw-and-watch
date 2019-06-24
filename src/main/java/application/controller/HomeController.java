69ee366707a6c07d173b4400b8f438f3c75a0174
Ivan Chernyak <ivanchernyak98@gmail.com>
2019-06-24 17:19:14 +0300

    added database-connection with jndi

--- src/main/java/application/controller/HomeController.java
+++ src/main/java/application/controller/HomeController.java
@@ -1,13 +1,23 @@
 package application.controller;
 
 import application.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.sql.SQLException;
+
 @Controller
 public class HomeController {
+
+	private JdbcTemplate jdbcTemplate;
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
 	@RequestMapping(value = {"/home", "", "/"}, method = RequestMethod.GET)
 	public String printHello(ModelMap model) {
 		User user = (User) model.get("user");
@@ -28,5 +38,15 @@ public class HomeController {
 		model.addAttribute("msg", "hello test");
 		model.addAttribute("user", new User());
 		return "registration";
+	}
+
+	@RequestMapping(value = "/jdbc", method = RequestMethod.GET)
+	public String jdbcTest(ModelMap model) {
+		try {
+			model.addAttribute("result", jdbcTemplate.getDataSource().getConnection().getSchema());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "jdbc-test";
 	}
 }
---
+++ web/WEB-INF/pages/jdbc-test.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>TEST</title>
+</head>
+<body>
+<p>User-schema: <c:out value="${result}"/> </p>
+
+</body>
+</html>
